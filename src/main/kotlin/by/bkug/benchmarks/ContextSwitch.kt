c67b2986603ac98e651128971970496aa7d641c3
Ruslan Ibragimov <ruslan@ibragimov.by>
2019-09-30 01:09:39 +0300

    Update versions, use single thread executor

--- src/main/kotlin/by/bkug/benchmarks/ContextSwitch.kt
+++ src/main/kotlin/by/bkug/benchmarks/ContextSwitch.kt
@@ -1,28 +1,45 @@
 package by.bkug.benchmarks
 
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.openjdk.jmh.annotations.Benchmark
-import org.openjdk.jmh.annotations.Param
 import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
 import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.TearDown
 import java.lang.Thread.sleep
+import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 @State(Scope.Thread)
 open class ContextSwitch {
-    @Param("1", "2", "4", "8", "16", "32", "64", "128", "256")
-    var time: Long = 0
+//    @Param("1", "2", "4", "8", "16", "32", "64", "128", "256")
+    var time: Long = 1
+
+    var ste: ExecutorCoroutineDispatcher? = null
+
+    @Setup
+    open fun setup() {
+        println("Setup")
+        ste = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+    }
+
+    @TearDown
+    open fun tearDown() {
+        ste?.close()
+    }
 
     @Benchmark
     open fun coroutines() {
+        val dispatcher = ste!!
         val jobs = List(10000) {
-            GlobalScope.launch(Dispatchers.IO) {
-                repeat(10) {
+            GlobalScope.launch(dispatcher) {
+                repeat(1000) {
                     delay(time)
                 }
             }
@@ -37,7 +54,7 @@ open class ContextSwitch {
     open fun threads() {
         val threads = List(10000) {
             thread(start = true) {
-                repeat(10) {
+                repeat(1000) {
                     sleep(time)
                 }
             }
